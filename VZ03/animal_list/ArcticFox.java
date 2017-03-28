75d8a401401767ce437909df39b5ef4bd96ae095
royyandzakiy <royyan.abdul@gmail.com>
2017-03-29 05:41:59 +0700

    animal_list add

---
+++ VZ03/animal_list/ArcticFox.java
@@ -0,0 +1,27 @@
+package animal_list;
+
+import animal.*;
+import location.Location;
+
+public class ArcticFox extends Animal {
+  public ArcticFox(){
+    this(20);
+  }
+  
+  public ArcticFox(int weight){
+    position = new Location();
+    classification = new Classification("Canidae","Vulpes","lagopus");
+    dietStatus = new DietStatus(weight,true,false);
+    habitatStatus = new HabitatStatus("land");
+  }
+
+  @Override
+  public String Interact() {
+	  return "ARCTIC RAWR";
+  }
+
+  @Override
+  public void Render() {
+    System.out.print('f');
+  }
+}
---
+++ VZ03/animal_list/BadakJawa.java
@@ -0,0 +1,27 @@
+package animal_list;
+
+import animal.*;
+import location.Location;
+
+public class BadakJawa extends Animal {
+  public BadakJawa(){
+    this(70);
+  }
+  
+  public BadakJawa(int weight){
+    position = new Location();
+    classification = new Classification("Rhinocerotidae","Rhinoceros","sondaicus");
+    dietStatus = new DietStatus(weight,true,false);
+    habitatStatus = new HabitatStatus("land");
+  }
+
+  @Override
+  public String Interact() {
+	  return "HMM DON'T MIND ME AND MY ENDANGERED STATUS";
+  }
+
+  @Override
+  public void Render() {
+    System.out.print('J');
+  }
+}
